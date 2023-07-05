88de5a94c0ddeaeca60c0177622ef074a51e13d4
Aleksander Dreszler <olek.d93@gmail.com>
2023-07-05 21:21:29 +0200

    Project created

---
+++ src/main/java/pl/adreszler/budgetmanager/BudgetManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.adreszler.budgetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/adreszler/budgetmanager/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adreszler.budgetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
